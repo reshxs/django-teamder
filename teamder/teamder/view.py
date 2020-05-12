9ed711479f8a7d6c000123737b9df06fd24b6f85
reshxs <56556713+reshxs@users.noreply.github.com>
2020-05-12 21:30:19 +0500

    Main Page indev
    Сделал суперудобную и красивую главнуб страничку временно, чтобы удобнее было работать с сайтом

--- teamder/teamder/view.py
+++ teamder/teamder/view.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Welcome to TeamDer!!! =)")
+    return render(request, 'base.html')
