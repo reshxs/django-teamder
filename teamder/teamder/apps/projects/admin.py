f7b58d801e7789857944ed09263b00b8ea9d9050
reshxs <56556713+reshxs@users.noreply.github.com>
2020-05-12 14:12:02 +0500

    14.11 commit

--- teamder/teamder/apps/projects/admin.py
+++ teamder/teamder/apps/projects/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 
-from .models import Project, Member, Ad, Technology
+from .models import Project, Ad, Technology
 
 admin.site.register(Project)
-admin.site.register(Member)
+# admin.site.register(Member)
 admin.site.register(Ad)
 admin.site.register(Technology)
--- teamder/teamder/apps/projects/models.py
+++ teamder/teamder/apps/projects/models.py
@@ -1,22 +1,10 @@
 import datetime
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.utils import timezone
 
 
-class Project(models.Model):
-    project_name = models.CharField('название проекта', max_length=100)
-    project_description = models.TextField('описание проекта')
-
-    def __str__(self):
-        return self.project_name
-
-    class Meta:
-        verbose_name = 'Проект'
-        verbose_name_plural = 'Проекты'
-
-
 class Ad(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     ad_pub_date = models.DateTimeField('дата публикации')
 
     # РАЗОБАРТЬСЯ ПОЧЕМУ НЕ РАБОТАИИИТ:
@@ -28,23 +16,39 @@ class Ad(models.Model):
         verbose_name_plural = 'Объявления'
 
 
-class Member(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    member_name = models.CharField('имя участника', max_length=50)
-    member_role = models.CharField('роль участника', max_length=50)
-
-    def __str__(self):
-        return self.member_name
-
-    class Meta:
-        verbose_name = 'Участник'
-        verbose_name_plural = 'Участники'
+#class Member(models.Model):
+#    member_name = models.CharField('имя участника', max_length=50)
+#    member_role = models.CharField('роль участника', max_length=50)
+#
+#    def __str__(self):
+#        return self.member_name
+#
+#    class Meta:
+#        verbose_name = 'Участник'
+#        verbose_name_plural = 'Участники'
 
 
 class Technology(models.Model):
-    project = models.ForeignKey(Project, on_delete=models.CASCADE)
     technology_name = models.CharField("название технологии", max_length=20)
 
     class Meta:
         verbose_name = 'Технология'
         verbose_name_plural = 'Технологии'
+
+
+class Project(models.Model):
+    project_name = models.CharField('название проекта', max_length=100)
+    project_description = models.TextField('описание проекта')
+    project_members = models.ManyToManyField(get_user_model())
+    project_technologies = models.ManyToManyField(Technology)
+    default_Ad = Ad
+    default_Ad.ad_pub_date = datetime.datetime.now()
+    project_Ad = models.OneToOneField(Ad, on_delete=models.CASCADE, default=default_Ad)
+
+
+    def __str__(self):
+        return self.project_name
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
