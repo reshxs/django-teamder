8852db0b219b4f70ab57d3d2edb9b5a4d7994b17
lapshinaanastasia <56558341+lapshinaanastasia@users.noreply.github.com>
2020-05-12 22:54:16 +0500

    newviews

--- teamder/teamder/apps/projects/views.py
+++ teamder/teamder/apps/projects/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'projects/list.html')
+
+def detail(request, project_id):
+ 	return render(request, 'project/detail.html')
