4814baf931926ca15fd521d9f73ff22b69bba8f2
reshxs <56556713+reshxs@users.noreply.github.com>
2020-05-13 23:03:48 +0500

    Super little fix

--- teamder/teamder/apps/projects/views.py
+++ teamder/teamder/apps/projects/views.py
@@ -3,16 +3,19 @@ from django.shortcuts import render
 
 from .models import Project, Member, Technology
 
+
 def index(request):
-	projects_list = Project.objects.order_by('-pub_date')
+    projects_list = Project.objects.order_by('-pub_date')
     return render(request, 'projects/list.html', {'projects_list': projects_list})
 
-def detail(request, project_id):
-	try:
-		a = Project.objects.get(id = project_id )
-	except:
-		raise Http404("Проект не найден!")
 
-	member_list = a.member_set.order_by()
-	technology_list = a.technology_set.order_by()
- 	return render(request, 'projects/detail.html', {'project': a, 'technology_list': technology_list, 'member_list': member_list})
+def detail(request, project_id):
+    try:
+        a = Project.objects.get(id=project_id)
+    except:
+        raise Http404("Проект не найден!")
+
+    member_list = a.member_set.order_by()
+    technology_list = a.technology_set.order_by()
+    return render(request, 'projects/detail.html',
+                  {'project': a, 'technology_list': technology_list, 'member_list': member_list})
--- teamder/teamder/apps/user_accounts/views.py
+++ teamder/teamder/apps/user_accounts/views.py
@@ -4,15 +4,16 @@ from django.shortcuts import render
 
 from .models import UserAccount
 
+
 def index(request):
-	user_accounts_list = UserAccount.object.order_by()
-    return render(request, 'user_accounts/list.html' {'user_accounts_list': user_accounts_list})
+    user_accounts_list = UserAccount.object.order_by()
+    return render(request, 'user_accounts/list.html', {'user_accounts_list': user_accounts_list})
 
 
 def detail(request, user_id):
     try:
-    	a = UserAccount.object.get(id = user_id)
+        a = UserAccount.object.get(id=user_id)
     except:
-    	raise Http404("Пользователь не найден!")
-    
-    return render(request, 'user_accounts/detail.html', {'user_accounts': a})
+        raise Http404("Пользователь не найден!")
+
+    return render(request, 'user_accounts/detail.html', {'user_account': a})
