860a45540e20a87bf970a1afcd370dd463bdaf80
reshxs <56556713+reshxs@users.noreply.github.com>
2020-06-02 15:53:43 +0500

    widget-tweaks!

--- teamder/teamder/settings.py
+++ teamder/teamder/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'user_accounts.apps.UserAccountsConfig',
+    'widget_tweaks',
     'projects.apps.ProjectsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
